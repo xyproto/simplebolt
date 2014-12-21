2ba255e101dd7500554b8f93bbaa42594264a5c1
Alexander F Rødseth <rodseth@gmail.com>
2014-12-21 20:17:02 +0100

    Minor changes to the example

--- example/main.go
+++ example/main.go
@@ -12,37 +12,38 @@ func main() {
 		log.Fatalln("Could not connect to Redis. Is the service up and running?")
 	}
 
-	// Use instead for testing if a different host/port is up
+	// Use instead for testing if a different host/port is up.
 	// simpleredis.TestConnectionHost("localhost:6379")
 
 	// Create a connection pool, connect to the given redis server
 	pool := simpleredis.NewConnectionPool()
 
-	// Use this for connecting to a different redis host/port (+password)
-	// pool := simpleredis.NewConnectionPoolHost("password@localhost:6379")
+	// Use this for connecting to a different redis host/port
+	// pool := simpleredis.NewConnectionPoolHost("localhost:6379")
 
-	// Close the connection pool when this function returns
+	// For connecting to a different redis host/port, with a password
+	// pool := simpleredis.NewConnectionPoolHost("password@redishost:6379")
+
+	// Close the connection pool right after this function returns
 	defer pool.Close()
 
 	// Create a list named "greetings"
 	list := simpleredis.NewList(pool, "greetings")
 
-	// Add "hello" to the list
-	err := list.Add("hello")
-	if err != nil {
+	// Add "hello" to the list, check if there are errors
+	if list.Add("hello") != nil {
 		log.Fatalln("Could not add an item to list!")
 	}
 
-	// Get the last item in the list
-	item, err := list.GetLast()
-	if err != nil {
+	// Get the last item of the list
+	if item, err := list.GetLast(); err != nil {
 		log.Fatalln("Could not fetch the last item from the list!")
+	} else {
+		log.Println("The value of the stored item is:", item)
 	}
-	log.Println("The value of the stored item is:", item)
 
 	// Remove the list
-	err = list.Remove()
-	if err != nil {
+	if list.Remove() != nil {
 		log.Fatalln("Could not remove the list!")
 	}
 }
