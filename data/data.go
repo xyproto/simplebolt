172af28eca9b2ce06ca0dc132797e02263541a1d
Luis Villegas <cheesetris21@gmail.com>
2020-01-04 19:03:59 -0430

    Add interface definition for accessing containers' data

---
+++ data/data.go
@@ -0,0 +1,11 @@
+// Package data defines the common set of methods among containers
+// to get and set the values of their underlying data.
+
+package data
+
+// StoredData is the set of methods that provides access to every container.
+type StoredData interface {
+	Value() []byte
+	Update([]byte) error
+	Delete() error
+}
